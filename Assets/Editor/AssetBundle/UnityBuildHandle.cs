befec1ab0f1ae9fbd194e5c24bf1760a9b30d962
wangjiangrong <903036544@qq.com>
2018-01-18 13:06:01 +0800

    unity build 前后触发的事件接口

---
+++ Assets/Editor/AssetBundle/UnityBuildHandle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+//using UnityEditor.Callbacks;
+using UnityEditor.Build;
+
+namespace Utility {
+
+	public class UnityBuildHandle : MonoBehaviour, IPreprocessBuild, IPostprocessBuild {
+
+        //[PostProcessBuild]
+        //public static void qwe(BuildTarget BuildTarget, string path)
+        //{
+        //    Debug.Log("项目build完成执行");
+        //}
+
+
+        public int callbackOrder { get { return 0; } }
+        public void OnPreprocessBuild(BuildTarget target, string path) {
+            Debug.Log("项目build前运行");
+        }
+
+        public void OnPostprocessBuild(BuildTarget target, string path) {
+            Debug.Log("项目build完成执行");
+        }
+    }
+}
