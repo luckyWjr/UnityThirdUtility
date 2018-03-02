4ac89a6eddc255f663cf906f11d700551f9b5b29
wangjiangrong <903036544@qq.com>
2018-03-02 15:30:59 +0800

    官方NavMeshComponents 添加

---
+++ Assets/Scripts/Nav/BaseNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.AI;
+using UnityEngine;
+
+namespace Nav {
+
+	public class BaseNavigation : MonoBehaviour {
+
+        //寻路目标
+        [SerializeField]Transform m_target;
+
+        //寻路组件
+        NavMeshAgent m_agent;
+
+        void Start () {
+            m_agent = GetComponent<NavMeshAgent>();
+        }
+		
+		void Update () {
+            //设置寻路
+            if(m_agent && m_target) {
+                m_agent.SetDestination(m_target.position);
+            }
+        }
+	}
+}
